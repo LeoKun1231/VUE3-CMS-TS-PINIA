242e2a8afea35e06930c7d2c8379d95979f8052d
Leo <1024983409@qq.com>
2023-09-11 18:07:34 +0800

    fix: 修改vercel部署地址

--- src/views/login/c-cpns/pane-account.vue
+++ src/views/login/c-cpns/pane-account.vue
@@ -2,7 +2,7 @@
  * @Author: Leo l024983409@qq.com
  * @Date: 2023-01-18 12:59:07
  * @LastEditors: Leo l024983409@qq.com
- * @LastEditTime: 2023-09-11 17:53:14
+ * @LastEditTime: 2023-09-11 18:03:45
  * @FilePath: \VUE3-CMS-TS-PINIA\src\views\login\c-cpns\pane-account.vue
  * @Description: 
 -->
@@ -14,7 +14,7 @@ import type { FormInstance, FormRules } from 'element-plus'
 
 //本地存储帐号密码
 const accountStorage = useLocalStorage('account', {
-  name: 'coderwhy',
+  name: 'coderwhy1',
   password: '123456'
 })
 
