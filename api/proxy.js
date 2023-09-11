2a7ba659592305a127bd5ba64b34fe6aaa75c11a
Leo <l024983409@qq.com>
2023-09-11 18:07:34 +0800

    fix: 修改vercel部署地址

--- api/proxy.js
+++ api/proxy.js
@@ -1,8 +1,8 @@
 /*
  * @Author: hqk
  * @Date: 2023-01-18 16:12:46
- * @LastEditors: hqk
- * @LastEditTime: 2023-01-18 16:16:44
+ * @LastEditors: Leo l024983409@qq.com
+ * @LastEditTime: 2023-09-11 18:06:43
  * @Description:
  */
 const { createProxyMiddleware } = require('http-proxy-middleware')
@@ -10,7 +10,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
   let target = '' // 如果请求以/proxy开头，则将代理转发的目标设为需要请求的http接口
   if (req.url.startsWith('/proxy')) {
-    target = 'http://111.230.245.205:8880'
+    target = import.meta.env.VITE_BASE_API
   } // 创建代理对象并转发请求
 
   createProxyMiddleware({
