c9b600480a13a12c95ebf8761e271f82b81648c2
Leo <1024983409@qq.com>
2023-12-18 20:13:22 +0800

    fix: 接口不同

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git filter-branch -f --env-filter '
-     GIT_AUTHOR_EMAIL="1024983409@qq.com";
-     GIT_AUTHOR_NAME="Leo";
-     GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL;
-     GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME";' -- --all
